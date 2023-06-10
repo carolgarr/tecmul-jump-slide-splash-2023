5580eaf5bac630660e1a43d2d6efc4baacf8867e
Diogo Marques <randombidoof@gmail.com>
2023-06-10 03:56:12 +0100

    camara não se move quando morres

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public new Transform camera;
+    public new Camera camera;
     Vector3 forward;
     Vector3 right;
+
     [HideInInspector]
     public Vector3 spawn;
 
@@ -16,42 +17,47 @@ public class Player : MonoBehaviour
     //Jump
     public float height;
     public float gravity;
-    private bool canJump = true;
+    private bool canJump;
+    private bool canPlay;
     
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
         spawn = transform.position;
+        canJump = true;
+        canPlay = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Walk
-        float horizontalValue = Input.GetAxisRaw("Horizontal");
-        float verticalValue = Input.GetAxisRaw("Vertical");
+        if(canPlay){
+            float horizontalValue = Input.GetAxisRaw("Horizontal");
+            float verticalValue = Input.GetAxisRaw("Vertical");
 
-        Vector3 forward = new Vector3(camera.transform.forward.x, 0.0f, camera.transform.forward.z).normalized;
-        Vector3 right = new Vector3(camera.transform.right.x, 0.0f, camera.transform.right.z).normalized;
-        
-        _rigidBody.AddForce(forward * speed * verticalValue);
-        _rigidBody.AddForce(right * speed * horizontalValue);
+            Vector3 forward = new Vector3(camera.transform.forward.x, 0.0f, camera.transform.forward.z).normalized;
+            Vector3 right = new Vector3(camera.transform.right.x, 0.0f, camera.transform.right.z).normalized;
+            
+            _rigidBody.AddForce(forward * speed * verticalValue);
+            _rigidBody.AddForce(right * speed * horizontalValue);
 
-        if(Input.GetButton("Jump") && canJump){
-            _rigidBody.AddForce(Vector3.up * height, ForceMode.Impulse);
-            canJump = false;
-        }
-        else {
-            _rigidBody.AddForce(Vector3.down * gravity);
+            if(Input.GetButton("Jump") && canJump){
+                _rigidBody.AddForce(Vector3.up * height, ForceMode.Impulse);
+                canJump = false;
+            }
+            else {
+                _rigidBody.AddForce(Vector3.down * gravity);
+            }
         }
 
         
     }
 
     void Update(){
-        if(transform.position.y < -5) {
-            Respawn();
+        if(canPlay && transform.position.y < -5) {
+            canPlay = false;
+            Kill();
         }
     }
 
@@ -60,8 +66,18 @@ public class Player : MonoBehaviour
         canJump = true;
     }
 
-    public void Respawn(){
+    public void Kill(){
+        Camera cam = camera.gameObject.GetComponent<Camera>();
+        cam.canMove = false;
+        canPlay = false;
+        StartCoroutine(Respawn(2, cam));
+    }
+    IEnumerator Respawn(float delay, Camera camera)
+    {
+        yield return new WaitForSeconds(delay);
         transform.position = spawn;
         _rigidBody.velocity = new Vector3(0,0,0);
+        camera.canMove = true;
+        canPlay = true;
     }
 }
